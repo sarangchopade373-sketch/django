a48d8f8486bc61bc9092ad447dd806a86ea2dc78
sarangchopade308-hash07 <sarangchopade308@gmail.com>
2026-05-24 22:20:41 +0530

    Task management project

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import TaskViewSet, home
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', home, name='home'),
+    path('api/', include(router.urls)),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
@@ -6,3 +7,7 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+def home(request):
+    return render(request, 'index.html')
